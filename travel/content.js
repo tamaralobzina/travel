747bf3261f9017d4fa1f615c70efb891ca4d73e3
tamaralobzina@gmail.com <tamaralobzina@gmail.com>
2018-03-29 20:06:52 +0300

    adding content.js

---
+++ travel/content.js
@@ -0,0 +1,93 @@
+let content = [
+    {
+        id: 1,
+        region: 'Eastern Europe',
+        country: 'Ukraine',
+        city: 'Kiev',
+        countryDescription: 'content1',
+        photo: 'photoAttachedHere1',
+
+    },
+
+    {
+        id: 2,
+        region: 'Eastern Europe',
+        country: 'Ukraine',
+        city: 'Odessa',
+        countryDescription: 'content2',
+        photo: 'photoAttachedHere2',
+
+    },
+
+    {
+        id: 3,
+        region: 'Eastern Europe',
+        country: 'Ukraine',
+        city: 'Lviv',
+        countryDescription: 'content3',
+        photo: 'photoAttachedHere3',
+    },
+
+    {
+        id: 4,
+        region: 'Europe',
+        country: 'Ukraine',
+        city: 'Lviv',
+        countryDescription: 'content4',
+        photo: 'photoAttachedHere4',
+    },
+
+    {
+        id: 5,
+        region: 'Europe',
+        country: 'Czech Republic',
+        city: 'Prague',
+        countryDescription: 'content5',
+        photo: 'photoAttachedHere5',
+    },
+
+    {
+        id: 6,
+        region: 'Central Asia',
+        country: 'Turkey',
+        city: 'Istanbul',
+        countryDescription: 'content6',
+        photo: 'photoAttachedHere6',
+    },
+
+    {
+        id: 7,
+        region: 'Central Asia',
+        country: 'Georgia',
+        city: 'Tbilisi',
+        countryDescription: 'content7',
+        photo: 'photoAttachedHere7',
+    },
+
+    {
+        id: 8,
+        region: 'Asia',
+        country: 'India',
+        city: 'New Delhi',
+        countryDescription: 'content8',
+        photo: 'photoAttachedHere8',
+    },
+
+    {
+        id: 9,
+        region: 'Asia',
+        country: 'India',
+        city: 'Agra',
+        countryDescription: 'content9',
+        photo: 'photoAttachedHere9',
+    },
+
+    {
+        id: 10,
+        region: 'Asia',
+        country: 'Indonesia',
+        city: 'Bali',
+        countryDescription: 'content10',
+        photo: 'photoAttachedHere10',
+    },
+];
